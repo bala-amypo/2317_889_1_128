e3f2a76982f60e94b62cae19235818dea71e3111
bala-amypo <bala@amypo.in>
2025-12-17 08:17:26 +0000

    User commit

--- src/main/java/com/example/demo/entity/InvestorProfile.java
+++ src/main/java/com/example/demo/entity/InvestorProfile.java
@@ -1,3 +1,4 @@
+@Entity
 public class InvestorProfile {
     @Id
     private Long id;
@@ -8,6 +9,9 @@ public class InvestorProfile {
     private String email;
     private boolean active;
     private LocalDate createdAt;
+
+    public InvestorProfile() {
+    }
 
     public InvestorProfile(
         Long id,
@@ -23,5 +27,53 @@ public class InvestorProfile {
         this.email = email;
         this.active = active;
         this.createdAt = createdAt
+    }
+
+    public void getId() {
+        return this.id;
+    }
+
+    public Long setId(Long id) {
+        this.id = id;
+    }
+
+    public void getInvestorId() {
+        return this.investorId;
+    }
+
+    public String setInvestorId(String investorId) {
+        this.investorId = investorId;
+    }
+
+    public void getFullName() {
+        return this.fullName;
+    }
+
+    public String setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean getActive() {
+        return this.active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
     }
 }
