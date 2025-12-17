10492e1544d93b575595ae8cb515404554c338fd
bala-amypo <bala@amypo.in>
2025-12-17 08:12:25 +0000

    User commit

--- src/main/java/com/example/demo/entity/InvestorProfile.java
+++ src/main/java/com/example/demo/entity/InvestorProfile.java
@@ -3,5 +3,25 @@ public class InvestorProfile {
     private Long id;
     private String investorId;
     private String fullName;
-    
+    @Column(name=unique)
+    @Email(message="Invalid Email")
+    private String email;
+    private boolean active;
+    private LocalDate createdAt;
+
+    public InvestorProfile(
+        Long id,
+        String investorId,
+        String fullName,
+        String email,
+        boolean active,
+        LocalDate createdAt
+    ) {
+        this.id = id;
+        this.investorId = investorId;
+        this.fullName = fullName;
+        this.email = email;
+        this.active = active;
+        this.createdAt = createdAt
+    }
 }
