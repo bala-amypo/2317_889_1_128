048ab263261003e194c85fce79cee58c01c80d5e
bala-amypo <bala@amypo.in>
2025-12-18 16:39:48 +0000

    User commit

---
+++ src/main/java/com/example/demo/repository/HoldingRecordRepository.java
@@ -0,0 +1,16 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import com.example.demo.entity.HoldingRecord;
+import com.example.demo.entity.enums.AssetClassType;
+
+@Repository
+public interface HoldingRecordRepository extends JpaRepository<HoldingRecord, Long> {
+    List<HoldingRecord> findByInvestorId(Long investorId);
+    List<HoldingRecord> findByValueGreaterThan(Double value);
+    List<HoldingRecord> findByInvestorIdAndAssetClass(Long investorId, AssetClassType assetClass);
+}
