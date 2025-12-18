379a1b93ede65904567d6492a982aee70affc278
bala-amypo <bala@amypo.in>
2025-12-18 09:59:25 +0000

    User commit

--- src/main/java/com/example/demo/entity/AllocationSnapshotRecord.java
+++ src/main/java/com/example/demo/entity/AllocationSnapshotRecord.java
@@ -1,66 +1,66 @@
-// package com.example.demo.entity;
+package com.example.demo.entity;
 
-// import java.time.LocalDateTime;
+import java.time.LocalDateTime;
 
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Positive;
 
-// @Entity
-// public class AllocationSnapshotRecord {
-//     @Id
-//     private Long id;
-//     private Long investorId;
-//     private LocalDateTime snapShotDate;
-//     private double totalPortfolioValue;
-//     private String allocationString;
+@Entity
+public class AllocationSnapshotRecord {
+    @Id
+    private Long id;
+    private Long investorId;
+    private LocalDateTime snapShotDate;
+    @Positive
+    private double totalPortfolioValue;
+    private String allocationString;
 
-//     public AllocationSnapshotRecord(String allocationString, Long id, Long investorId, LocalDateTime snapShotDate, double totalPortfolioValue) {
-//         this.allocationString = allocationString;
-//         this.id = id;
-//         this.investorId = investorId;
-//         this.snapShotDate = snapShotDate;
-//         this.totalPortfolioValue = totalPortfolioValue;
-//     }
+    public AllocationSnapshotRecord(String allocationString, Long id, Long investorId, LocalDateTime snapShotDate, double totalPortfolioValue) {
+        this.allocationString = allocationString;
+        this.id = id;
+        this.investorId = investorId;
+        this.snapShotDate = snapShotDate;
+        this.totalPortfolioValue = totalPortfolioValue;
+    }
 
-//     public Long getId() {
-//         return id;
-//     }
+    public Long getId() {
+        return id;
+    }
 
-//     public void setId(Long id) {
-//         this.id = id;
-//     }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-//     public Long getInvestorId() {
-//         return investorId;
-//     }
+    public Long getInvestorId() {
+        return investorId;
+    }
 
-//     public void setInvestorId(Long investorId) {
-//         this.investorId = investorId;
-//     }
+    public void setInvestorId(Long investorId) {
+        this.investorId = investorId;
+    }
 
-//     public LocalDateTime getSnapShotDate() {
-//         return snapShotDate;
-//     }
+    public LocalDateTime getSnapShotDate() {
+        return snapShotDate;
+    }
 
-//     public void setSnapShotDate(LocalDateTime snapShotDate) {
-//         this.snapShotDate = snapShotDate;
-//     }
+    public void setSnapShotDate(LocalDateTime snapShotDate) {
+        this.snapShotDate = snapShotDate;
+    }
 
-//     public double getTotalPortfolioValue() {
-//         return totalPortfolioValue;
-//     }
+    public double getTotalPortfolioValue() {
+        return totalPortfolioValue;
+    }
 
-//     public void setTotalPortfolioValue(double totalPortfolioValue) {
-//         this.totalPortfolioValue = totalPortfolioValue;
-//     }
+    public void setTotalPortfolioValue(double totalPortfolioValue) {
+        this.totalPortfolioValue = totalPortfolioValue;
+    }
 
-//     public String getAllocationString() {
-//         return allocationString;
-//     }
+    public String getAllocationString() {
+        return allocationString;
+    }
 
-//     public void setAllocationString(String allocationString) {
-//         this.allocationString = allocationString;
-//     }
-
-
-// }
+    public void setAllocationString(String allocationString) {
+        this.allocationString = allocationString;
+    }
+}
--- src/main/java/com/example/demo/entity/AssestClassAllocationRule.java
+++ src/main/java/com/example/demo/entity/AssestClassAllocationRule.java
@@ -1,62 +1,66 @@
-// package com.example.demo.entity;
+package com.example.demo.entity;
 
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 
-// @Entity
-// public class AssetClassAllocationRule {
-//     @Id
-//     private Long id;
-//     private Long investorId;
-//     private AssestClassType assestClass;
-//     private Double targetPercentage;
-//     private Boolean active;
+@Entity
+public class AssetClassAllocationRule {
+    @Id
+    private Long id;
+    private Long investorId;
+    private AssestClassType assestClass;
+    @Min(0)
+    @Max(100)
+    private Double targetPercentage;
+    private Boolean active;
 
-//     public AssetClassAllocationRule(Boolean active, AssestClassType assestClass, Long id, Long investorId, Double targetPercentage) {
-//         this.active = active;
-//         this.assestClass = assestClass;
-//         this.id = id;
-//         this.investorId = investorId;
-//         this.targetPercentage = targetPercentage;
-//     }
+    public AssetClassAllocationRule(Boolean active, AssestClassType assestClass, Long id, Long investorId, Double targetPercentage) {
+        this.active = active;
+        this.assestClass = assestClass;
+        this.id = id;
+        this.investorId = investorId;
+        this.targetPercentage = targetPercentage;
+    }
 
-//     public Long getId() {
-//         return id;
-//     }
+    public Long getId() {
+        return id;
+    }
 
-//     public void setId(Long id) {
-//         this.id = id;
-//     }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-//     public Long getInvestorId() {
-//         return investorId;
-//     }
+    public Long getInvestorId() {
+        return investorId;
+    }
 
-//     public void setInvestorId(Long investorId) {
-//         this.investorId = investorId;
-//     }
+    public void setInvestorId(Long investorId) {
+        this.investorId = investorId;
+    }
 
-//     public AssestClassType getAssestClass() {
-//         return assestClass;
-//     }
+    public AssestClassType getAssestClass() {
+        return assestClass;
+    }
 
-//     public void setAssestClass(AssestClassType assestClass) {
-//         this.assestClass = assestClass;
-//     }
+    public void setAssestClass(AssestClassType assestClass) {
+        this.assestClass = assestClass;
+    }
 
-//     public Double getTargetPercentage() {
-//         return targetPercentage;
-//     }
+    public Double getTargetPercentage() {
+        return targetPercentage;
+    }
 
-//     public void setTargetPercentage(Double targetPercentage) {
-//         this.targetPercentage = targetPercentage;
-//     }
+    public void setTargetPercentage(Double targetPercentage) {
+        this.targetPercentage = targetPercentage;
+    }
 
-//     public Boolean isActive() {
-//         return active;
-//     }
+    public Boolean isActive() {
+        return active;
+    }
 
-//     public void setActive(Boolean active) {
-//         this.active = active;
-//     }
-// }
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
