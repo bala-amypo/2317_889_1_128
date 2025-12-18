a98a31701e682df8c5eb0edd565662c29ab5b006
bala-amypo <bala@amypo.in>
2025-12-18 08:21:39 +0000

    User commit

--- src/main/java/com/example/demo/services/UserAccountService.java
+++ src/main/java/com/example/demo/services/UserAccountService.java
@@ -1,9 +1,10 @@
 package com.example.demo.services;
 
 import java.util.Optional;
-
+import org.springframework.stereotype.Service;
 import com.example.demo.entity.UserAccount;
 
+@Service
 public interface UserAccountService {
     UserAccount getUserDataFromDB(Long id);
     UserAccount postUserDateToDB(UserAccount userAccount);
