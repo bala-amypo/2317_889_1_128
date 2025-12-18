c3f173c2bad16691279fabbcdabf56b7ef7054f6
bala-amypo <bala@amypo.in>
2025-12-18 04:57:10 +0000

    User commit

--- src/main/java/com/example/demo/entity/UserAccount.java
+++ src/main/java/com/example/demo/entity/UserAccount.java
@@ -1,79 +1,79 @@
-package com.example.demo.entity;
+// package com.example.demo.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.Email;
+// import jakarta.persistence.Column;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.Id;
+// import jakarta.validation.constraints.Email;
 
-@Entity
-public class UserAccount {
-    @Id
-    private Long id;
-    @Column(unique=true)
-    private String userName;
-    @Column(unique=true)
-    @Email(message="Invalid email")
-    private String email;
-    private String password;
-    private RoleType role;
-    private Boolean active;
+// @Entity
+// public class UserAccount {
+//     @Id
+//     private Long id;
+//     @Column(unique=true)
+//     private String userName;
+//     @Column(unique=true)
+//     @Email(message="Invalid email")
+//     private String email;
+//     private String password;
+//     private RoleType role;
+//     private Boolean active;
 
-    public UserAccount(Boolean active, String email, Long id, String password, RoleType role, String userName) {
-        this.active = active;
-        this.email = email;
-        this.id = id;
-        this.password = password;
-        this.role = role;
-        this.userName = userName;
-    }
+//     public UserAccount(Boolean active, String email, Long id, String password, RoleType role, String userName) {
+//         this.active = active;
+//         this.email = email;
+//         this.id = id;
+//         this.password = password;
+//         this.role = role;
+//         this.userName = userName;
+//     }
 
-    public Long getId() {
-        return id;
-    }
+//     public Long getId() {
+//         return id;
+//     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+//     public void setId(Long id) {
+//         this.id = id;
+//     }
 
-    public String getUserName() {
-        return userName;
-    }
+//     public String getUserName() {
+//         return userName;
+//     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
+//     public void setUserName(String userName) {
+//         this.userName = userName;
+//     }
 
-    public String getEmail() {
-        return email;
-    }
+//     public String getEmail() {
+//         return email;
+//     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+//     public void setEmail(String email) {
+//         this.email = email;
+//     }
 
-    public String getPassword() {
-        return password;
-    }
+//     public String getPassword() {
+//         return password;
+//     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+//     public void setPassword(String password) {
+//         this.password = password;
+//     }
 
-    public RoleType getRole() {
-        return role;
-    }
+//     public RoleType getRole() {
+//         return role;
+//     }
 
-    public void setRole(RoleType role) {
-        this.role = role;
-    }
+//     public void setRole(RoleType role) {
+//         this.role = role;
+//     }
 
-    public Boolean getActive() {
-        return active;
-    }
+//     public Boolean getActive() {
+//         return active;
+//     }
 
-    public void setActive(Boolean active) {
-        this.active = active;
-    }
+//     public void setActive(Boolean active) {
+//         this.active = active;
+//     }
 
 
-}
+// }
