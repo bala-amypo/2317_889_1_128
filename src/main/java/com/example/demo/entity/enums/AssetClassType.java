d289b2bd05eb712e3c1c2b629a31bca62e8eb8b6
bala-amypo <bala@amypo.in>
2025-12-18 05:07:09 +0000

    User commit

--- src/main/java/com/example/demo/entity/enums/AssetClassType.java
+++ src/main/java/com/example/demo/entity/enums/AssetClassType.java
@@ -1,5 +1,6 @@
 package com.example.demo.entity.enums;
 
 public enum AssetClassType {
-    STOCKS, BONDS, CRYPTO, REAL_ESTATE_CASH, OTHER
+    STOCKS, 
+    BONDS, CRYPTO, REAL_ESTATE_CASH, OTHER
 }
