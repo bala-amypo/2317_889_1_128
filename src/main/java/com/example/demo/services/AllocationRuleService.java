140a2dec12b4e456fa822029c9264803d094041d
bala-amypo <bala@amypo.in>
2025-12-18 16:29:41 +0000

    User commit

---
+++ src/main/java/com/example/demo/services/AllocationRuleService.java
@@ -0,0 +1,14 @@
+package com.example.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.demo.entity.AssetClassAllocationRule;
+
+public interface AllocationRuleService {
+    AssetClassAllocationRule createRule(AssetClassAllocationRule rule);
+    Optional<AssetClassAllocationRule> updateRule(Long id, AssetClassAllocationRule updatedRule);
+    List<AssetClassAllocationRule> getRulesByInvestor(Long investorId);
+    List<AssetClassAllocationRule> getActiveRules(Long investorId);
+    Optional<AssetClassAllocationRule> getRuleById(Long id);
+}
---
+++ src/main/java/com/example/demo/services/impl/AllocationRuleServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.demo.services.impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.AssetClassAllocationRule;
+import com.example.demo.repository.AssetClassAllocationRuleRepository;
+import com.example.demo.services.AllocationRuleService;
+
+@Service
+public class AllocationRuleServiceImpl implements AllocationRuleService {
+    private final AssetClassAllocationRuleRepository assetClassAllocationRuleRepository;
+
+    public AllocationRuleServiceImpl(AssetClassAllocationRuleRepository assetClassAllocationRuleRepository) {
+        this.assetClassAllocationRuleRepository = assetClassAllocationRuleRepository;
+    }
+
+    @Override
+    public AssetClassAllocationRule createRule(AssetClassAllocationRule rule) {
+        assetClassAllocationRuleRepository.saveAndFlush(rule);
+        return rule;
+    }
+
+    @Override
+    public Optional<AssetClassAllocationRule> updateRule(Long id, AssetClassAllocationRule updatedRule) {
+        assetClassAllocationRuleRepository.deleteById(id);
+        assetClassAllocationRuleRepository.saveAndFlush(updatedRule);
+        return assetClassAllocationRuleRepository.findById(id);
+    }
+
+    @Override
+    public List<AssetClassAllocationRule> getRulesByInvestor(Long investorId) {
+        return assetClassAllocationRuleRepository.findActiveRulesHql(investorId);
+    }
+    
+    @Override
+    public List<AssetClassAllocationRule> getActiveRules(Long investorId) {
+        return assetClassAllocationRuleRepository.findActiveRulesHql(investorId);
+    }
+
+    @Override
+    public Optional<AssetClassAllocationRule> getRuleById(Long id) {
+        return assetClassAllocationRuleRepository.findById(id);
+    }
+}
