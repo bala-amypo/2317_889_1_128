20e22e90036b192ce52a67b07b512acdbc06802d
bala-amypo <bala@amypo.in>
2025-12-21 02:23:35 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig {
                     "/status",
                     "/api/**"
                 ).permitAll()
-                .requestMatchers("/api/**").authenticated()
+                // .requestMatchers("/api/**").authenticated()
             )
             .addFilterBefore(
                 jwtAuthenticationFilter,
