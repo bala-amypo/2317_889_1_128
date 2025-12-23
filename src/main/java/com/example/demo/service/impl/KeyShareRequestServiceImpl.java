2da79a89a604e207bdbee293447653f4b699f502
bala-amypo <bala@amypo.in>
2025-12-23 09:30:12 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/KeyShareRequestServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/KeyShareRequestServiceImpl.java
@@ -1,20 +1,31 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.model.KeyShareRequest;
-import com.example.demo.repository.KeyShareRequestRepository;
-import com.example.demo.service.KeyShareRequestService;
-import org.springframework.stereotype.Service;
-
 import java.time.Instant;
 import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.KeyShareRequest;
+import com.example.demo.repository.DigitalKeyRepository;
+import com.example.demo.repository.GuestRepository;
+import com.example.demo.repository.KeyShareRequestRepository;
+import com.example.demo.service.KeyShareRequestService;
 
 @Service
 public class KeyShareRequestServiceImpl implements KeyShareRequestService {
 
     private final KeyShareRequestRepository keyShareRequestRepository;
+    private final DigitalKeyRepository digitalKeyRepository;
+    private final GuestRepository guestRepository;
 
-    public KeyShareRequestServiceImpl(KeyShareRequestRepository keyShareRequestRepository) {
+    public KeyShareRequestServiceImpl(
+            KeyShareRequestRepository keyShareRequestRepository,
+            DigitalKeyRepository digitalKeyRepository,
+            GuestRepository guestRepository) {
+
         this.keyShareRequestRepository = keyShareRequestRepository;
+        this.digitalKeyRepository = digitalKeyRepository;
+        this.guestRepository = guestRepository;
     }
 
     @Override
@@ -24,11 +35,11 @@ public class KeyShareRequestServiceImpl implements KeyShareRequestService {
         Instant end = req.getShareEnd();
 
         if (end.isBefore(start)) {
-            throw new IllegalArgumentException("Share end before start");
+            throw new IllegalArgumentException("Share end");
         }
 
         if (req.getSharedBy().getId().equals(req.getSharedWith().getId())) {
-            throw new IllegalArgumentException("sharedBy and sharedWith cannot be same");
+            throw new IllegalArgumentException("sharedBy and sharedWith");
         }
 
         return keyShareRequestRepository.save(req);
