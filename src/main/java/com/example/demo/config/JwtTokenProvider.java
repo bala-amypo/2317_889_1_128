8f15ba27155330fd4894e6bcfebd8c6ffa16c416
bala-amypo <bala@amypo.in>
2025-12-26 17:44:02 +0000

    User commit

--- src/main/java/com/example/demo/config/JwtTokenProvider.java
+++ src/main/java/com/example/demo/config/JwtTokenProvider.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 
 import com.example.demo.entity.UserAccount;
 
+@Component
 public class JwtTokenProvider {
     private final String secret;
     private final long validity;
@@ -30,72 +31,3 @@ public class JwtTokenProvider {
         return token.split("-")[0];
     }
 }
-
-// package com.example.demo.config;
-
-// import java.util.Date;
-
-// import org.springframework.security.core.Authentication;
-// import org.springframework.stereotype.Component;
-
-// import com.example.demo.entity.UserAccount;
-
-// import io.jsonwebtoken.Jwts;
-// import io.jsonwebtoken.SignatureAlgorithm;
-
-// @Component
-// public class JwtTokenProvider {
-
-//     private final String secret;
-//     private final long validity;
-
-//     public JwtTokenProvider(String secret, long validity) {
-//         this.secret = secret;
-//         this.validity = validity;
-//     }
-
-//     public String generateToken(Authentication auth, UserAccount user) {
-
-//         String jwt = Jwts.builder()
-//                 .setSubject(user.getEmail())
-//                 .claim("role", user.getRole().name())
-//                 .setIssuedAt(new Date())
-//                 .setExpiration(new Date(System.currentTimeMillis() + validity))
-//                 .signWith(SignatureAlgorithm.HS256, secret)
-//                 .compact();
-
-//         return auth.getName() + "-token." + jwt;
-//     }
-
-//     public boolean validateToken(String token) {
-
-//         if (token == null || !token.contains("-token")) {
-//             return false;
-//         }
-
-//         if (!token.contains(".")) {
-//             return token.matches("^[a-zA-Z]+[0-9]*-token$");
-//         }
-
-//         try {
-//             String jwt = token.split("\\.", 2)[1];
-
-//             Jwts.parser()
-//                 .setSigningKey(secret)
-//                 .parseClaimsJws(jwt);
-
-//             return true;
-//         } catch (Exception e) {
-//             return false;
-//         }
-//     }
-
-//     public String getUsernameFromToken(String token) {
-
-//         if (!token.contains(".")) {
-//             return token.split("-")[0];
-//         }
-
-//         return token.split("-token")[0];
-//     }
-// }
