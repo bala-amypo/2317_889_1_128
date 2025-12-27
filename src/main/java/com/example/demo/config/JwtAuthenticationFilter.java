bec7860299356629827d77f9d8410a6cfd604c4b
bala-amypo <bala@amypo.in>
2025-12-27 05:40:19 +0000

    User commit

--- src/main/java/com/example/demo/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/demo/config/JwtAuthenticationFilter.java
@@ -2,7 +2,6 @@ package com.example.demo.config;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Collections;
 
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -22,76 +21,43 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
-    // @Override
-    // protected void doFilterInternal(
-    //         HttpServletRequest request,
-    //         HttpServletResponse response,
-    //         FilterChain filterChain)
-    //         throws ServletException, IOException {
-
-    //     String header = request.getHeader("Authorization");
-
-    //     if (header != null && header.startsWith("Bearer ")) {
-
-    //         String token = header.substring(7);
-
-    //         if (jwtTokenProvider.validateToken(token)) {
-
-    //             String username =
-    //                     jwtTokenProvider.getUsernameFromToken(token);
-
-    //             String role = jwtTokenProvider.getRoleFromToken(token);
-
-    //             SimpleGrantedAuthority authority =
-    //                 new SimpleGrantedAuthority("ROLE_" + role);
-
-
-    //             UsernamePasswordAuthenticationToken authentication =
-    //                     new UsernamePasswordAuthenticationToken(
-    //                             username,
-    //                             null,
-    //                             List.of(authority)
-    //                     );
-
-    //             SecurityContextHolder
-    //                     .getContext()
-    //                     .setAuthentication(authentication);
-    //         }
-    //     }
-
-    //     filterChain.doFilter(request, response);
-    // }
-
     @Override
-    protected void doFilterInternal(HttpServletRequest request, 
-                                   HttpServletResponse response, 
-                                   FilterChain filterChain) 
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain)
             throws ServletException, IOException {
-        
-        String token = extractToken(request);
-        
-        if (token != null && jwtTokenProvider.validateToken(token)) {
-            String email = jwtTokenProvider.getEmailFromToken(token);
-            String role = jwtTokenProvider.getRoleFromToken(token);
-            
-            UsernamePasswordAuthenticationToken authentication = 
-                    new UsernamePasswordAuthenticationToken(
-                            email, 
-                            null, 
-                            Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role))
-                    );
-            
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String header = request.getHeader("Authorization");
+
+        if (header != null && header.startsWith("Bearer ")) {
+
+            String token = header.substring(7);
+
+            if (jwtTokenProvider.validateToken(token)) {
+
+                String username =
+                        jwtTokenProvider.getUsernameFromToken(token);
+
+                String role = jwtTokenProvider.getRoleFromToken(token);
+
+                SimpleGrantedAuthority authority =
+                    new SimpleGrantedAuthority("ROLE_" + role);
+
+
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(
+                                username,
+                                null,
+                                List.of(authority)
+                        );
+
+                SecurityContextHolder
+                        .getContext()
+                        .setAuthentication(authentication);
+            }
         }
-        
+
         filterChain.doFilter(request, response);
-    }
-    
-    private String extractToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
-        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7);
-        }
-        return null;
     }
 }
