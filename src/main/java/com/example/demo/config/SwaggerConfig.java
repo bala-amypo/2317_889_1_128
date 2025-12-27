9bc8c05297947ae0d530e023d1ff65cf0da00266
bala-amypo <bala@amypo.in>
2025-12-27 04:06:43 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -15,23 +15,6 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 @Configuration
 public class SwaggerConfig {
 
-   @Bean
-    public OpenAPI openAPI() {
-
-        SecurityScheme bearerAuth = new SecurityScheme()
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT");
-
-        return new OpenAPI()
-                .components(
-                    new Components().addSecuritySchemes("bearerAuth", bearerAuth)
-                )
-                .addSecurityItem(
-                    new SecurityRequirement().addList("bearerAuth")
-                );
-    }     
-
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
